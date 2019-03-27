62ff012eef6292e33bae9e8a1f2d79ea03506f4e
Juan Diego <juandiegosophos@gmail.com>
2019-03-27 10:13:41 -0500

    Linea base

---
+++ src/main/java/com/sophos/poc/WsRestLoginApplication.java
@@ -0,0 +1,13 @@
+package com.sophos.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRestLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsRestLoginApplication.class, args);
+	}
+
+}
