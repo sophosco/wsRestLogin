cb02cae3360e67928e28dd18a132ca3579e15828
Juan Diego <juandiegosophos@gmail.com>
2019-03-27 20:20:55 -0500

    Adicion de Headers CORS

--- src/main/java/com/sophos/poc/login/WsRestLoginApplication.java
+++ src/main/java/com/sophos/poc/login/WsRestLoginApplication.java
@@ -2,7 +2,9 @@ package com.sophos.poc.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class WsRestLoginApplication {
 
